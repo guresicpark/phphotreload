e638fdec6bcb2bba32feb931002ed2d3187defef
Obrad Guresic <oguresic@googlemail.com>
2017-10-27 14:45:07 +0200

    Extension update

--- chrome_extension/js/background.js
+++ chrome_extension/js/background.js
@@ -1,16 +1,16 @@
 // Kindly provided by guresicpark.com
 
 // Global variables
-var log = console.log.bind(console);
-var socket = io.connect('http://localhost:1337');
-var blServerConnection = false,
-    gliCurrentTabId = void 0;
+var log = console.log.bind(console),
+    gloSocket = io.connect('http://localhost:1337'),
+    glblServerConnection = false,
+    gliTabIdLast = void 0;
 
-socket.on('connect', function() {
+gloSocket.on('connect', function() {
 
     // send current active tab
     chrome.tabs.getSelected(null, function(poActiveTab){
-        socket.send(JSON.stringify({
+        gloSocket.send(JSON.stringify({
             sUrl: poActiveTab.url,
             iTabId: poActiveTab.id,
             sMessage: "initialized"
@@ -18,28 +18,26 @@ socket.on('connect', function() {
     });
 
     // change phphotreload icon
-    blServerConnection = true;
+    glblServerConnection = true;
     chrome.browserAction.setIcon({path: "img/icon_green_16.png"});
 });
 
-socket.on('message', function (jsonDataFromServer) {
+gloSocket.on('message', function (jsonDataFromServer) {
     var aData = JSON.parse(jsonDataFromServer);
     var iTabId = aData.iTabId;
     if (typeof iTabId === 'undefined' || !iTabId) {
-		return;
+		iTabId = gliTabIdLast;
     }
-    chrome.browserAction.setIcon({path: "img/icon_red_16.png"});
-    try {
+    if (typeof iTabId !== 'undefined' && iTabId) {
+        chrome.browserAction.setIcon({path: "img/icon_red_16.png"});
         chrome.tabs.reload(iTabId, {bypassCache: true});
-    } catch (e) {
-        // fallback
-        chrome.tabs.executeScript(iTabId, {code: 'window.location.reload()'});
-    }
+        gliTabIdLast = iTabId;	
+	}
 });
 
-socket.on('disconnect', function () {
+gloSocket.on('disconnect', function () {
     // change phphotreload icon
-    blServerConnection = false;
+    glblServerConnection = false;
     chrome.browserAction.setIcon({path: "img/icon_yellow_16.png"});
 });
 
@@ -47,27 +45,23 @@ socket.on('disconnect', function () {
 // After tab was reloaded or a link was clicked
  chrome.tabs.onUpdated.addListener(function (pTabId, poChangeInfo, poTab) {
     if(poChangeInfo.status == "complete"){
-        // chrome.tabs.getSelected(null, function(poActiveTab){
-            // if (poActiveTab.id == poTab.id) {
-                if (blServerConnection) {
-                    chrome.browserAction.setIcon({path: "img/icon_green_16.png"});
-                } else {
-                    chrome.browserAction.setIcon({path: "img/icon_yellow_16.png"});
-                }
-                socket.send(JSON.stringify({
-                    sUrl: poTab.url,
-                    iTabId: poTab.id,
-                    sMessage: "reloaded"
-                }));
-            // }
-        // });
+        if (glblServerConnection) {
+            chrome.browserAction.setIcon({path: "img/icon_green_16.png"});
+        } else {
+            chrome.browserAction.setIcon({path: "img/icon_yellow_16.png"});
+        }
+        gloSocket.send(JSON.stringify({
+            sUrl: poTab.url,
+            iTabId: poTab.id,
+            sMessage: "reloaded"
+        }));
     }
 });
 
 // Tab was switched
 chrome.tabs.onActivated.addListener(function (poActiveInfo) {
     chrome.tabs.get(poActiveInfo.tabId, function(pTab){
-        socket.send(JSON.stringify({
+        gloSocket.send(JSON.stringify({
             sUrl: pTab.url,
             iTabId: poActiveInfo.tabId,
             sMessage: "switched"
